be70f51c32f10d8cc020b7f3aaea576dd648ed2a
rdubowski <rafaldud@gmail.com>
2021-04-11 19:27:31 +0200

    Add django admin site

--- quiz/models.py
+++ quiz/models.py
@@ -59,12 +59,12 @@ class Question(Updated):
 
 
 class Answer(Updated):
-    question = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+    question = models.ForeignKey(Question, default=1, on_delete=models.DO_NOTHING)
     answer_text = models.CharField(max_length=255, verbose_name=_("Quiz Title"))
     is_right = models.BooleanField(default=False)
        
     def __str__(self):
-        return self.title
+        return self.answer_text
    
    
     class Meta:
