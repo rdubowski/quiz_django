a18247cb089028ea5eefdc283d891bf2bb782849
rdubowski <rafaldud@gmail.com>
2021-04-12 08:32:40 +0200

    Add QuizListView and RandomQuestionView

---
+++ quiz/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import Quiz
+app_name = 'quiz'
+
+urlpatterns = [
+    path('', Quiz.as_view(), name='quiz'),
+    path('r/<str:topic>', RandomQuestion.as_view(), name='random'),
+]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Quizzes, Question
+from .serializers import QuizSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+class Quiz(generics.ListAPIView):
+    serializer_class = QuizSerializer
+    queryset = Quizzes.objects.all()
+
+
+class RandomQuestion(APIView):
+
+    def get(self, reequest, format:None, **kwargs):
+        question = Question.objects.filter(quiz__title=kwargs['topic']).order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many=True)
+        return Response(serializer.data)
